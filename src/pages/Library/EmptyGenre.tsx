4fd257178fd0c16f87a4b9c762a1b2136b1a75e8
Dave Aigbe Jr <dave.aigbe@outlook.com>
2023-01-27 05:57:56 -0600

    - Added SVG to EmptyGenre Component -

--- src/pages/Library/EmptyGenre.tsx
+++ src/pages/Library/EmptyGenre.tsx
@@ -4,14 +4,19 @@ interface Props {}
 
 const EmptyGenre: FunctionComponent<Props> = () => {
   return (
-    <section className={"flex items-center justify-center p-10"}>
+    <section className={"relative flex items-center justify-center p-10"}>
       <h2
         className={
-          "animate-pulse text-center text-3xl font-extrabold tracking-widest text-red-400"
+          "relative z-10 animate-bounce text-center text-3xl font-extrabold tracking-widest text-main-header"
         }
       >
         Genre is currently empty, add videos to display here...
       </h2>
+      <img
+        className={"absolute -right-2/4 z-0 w-1300"}
+        src={"/images/pink-loading.svg"}
+        alt={"Person grabbing globe shaped light bulb"}
+      />
     </section>
   );
 };
